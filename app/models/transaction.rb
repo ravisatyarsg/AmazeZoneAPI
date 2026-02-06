1dc68dbcadec77936ddddeeb1abaddb82a801ae1
ravisatyarsg <59511109+ravisatyarsg@users.noreply.github.com>
2026-02-06 18:39:26 -0500

    Add Transaction model with associations and callbacks

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,1 +1,17 @@
-
+class Transaction < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+  belongs_to :credit_card     
+  before_create :generate_transaction_number
+  before_create :calculate_total_cost
+  
+  private
+  
+  def generate_transaction_number
+    self.transaction_number = Array.new(10) { [*"A".."Z", *"0".."9"].sample }.join
+  end
+  
+  def calculate_total_cost
+    self.total_cost = product.price * quantity
+  end
+end
